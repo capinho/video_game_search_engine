2a0031525f9bc22655519f88dd6a728b4166d438
capinho <papadiawara@hotmail.fr>
2023-01-08 17:38:51 +0100

    correct bug on the main branch and error on the main commit name

---
+++ README.md
@@ -0,0 +1,13 @@
+# Template for the Video Game Search Engine project
+
+[![build workflow](https://github.com/capinho/video_game_search_engine/actions/workflows/build.yml/badge.svg)](https://github.com/capinho/video_game_search_engine/actions)
+[![codecov](https://codecov.io/gh/capinho/video_game_search_engine/branch/main/graph/badge.svg)](https://codecov.io/gh/capinho/video_game_search_engine)
+
+
+## Build the project
+
+The project requires a JDK 17 (recommended distribution is Temurin from [Adoptium](https://adoptium.net/)).
+
+From there, simply use the Maven wrapper to launch all tests (unit tests & integration tests).
+
+`./mvnw verify`
---
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -0,0 +1,41 @@
+package fr.lernejo.fileinjector;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class Launcher {
+    public static void main(String[] args) throws IOException {
+        try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
+
+            if (args.length > 0 && args[0] != "") {
+                ObjectMapper map = new ObjectMapper();
+                List<Game> games = Arrays.asList(map.readValue(Paths.get(args[0]).toFile(), Game[].class));
+                RabbitTemplate template = springContext.getBean(RabbitTemplate.class);
+                for (Game game : games) {
+//                    template.setMessageConverter(new Jackson2JsonMessageConverter());
+//                    template.convertAndSend("", "game_info", game, msg -> {
+//                        msg.getMessageProperties().setContentType(MessageProperties.CONTENT_TYPE_JSON);
+//                        msg.getMessageProperties().setHeader("game_id", game.id());
+//                        return msg;
+//                    });
+                    template.setMessageConverter(new Jackson2JsonMessageConverter());
+                    template.convertAndSend("", "game_info", game, msg -> {
+                        msg.getMessageProperties().getHeaders().put("game_id", game.id());
+                        return msg;
+                    });
+                }
+            } else { throw new IOException("No parameters passed!"); }
+        } catch (IOException e) { throw new IOException(e); }
+    }
+}
